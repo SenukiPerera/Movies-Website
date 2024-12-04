729ceb94538cf33beab301ebf10c834b7911079a
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2024-12-04 12:54:40 +0530

    home page updated

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let header = document.querySelector('header');
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll',() => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -37,5 +51,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     
   });
+
+
 
 
